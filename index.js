a1b1e950ffde95b5587dc609078b5406bef0e49b
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-12-23 16:05:50 -0200

    Add lib/tweets module
    - update index.js to export options

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 require('coffee-script/register');
 require('dotenv').load({silent: true});
+
+var tweets = require('./lib/tweets');
+
+module.exports = {
+  search: tweets.search,
+  stream: tweets.stream
+};
