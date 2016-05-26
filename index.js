a041cabe65c84c335b73cd64f31eaa2a7abbd28a
Paulo Diovani <paulo@diovani.com>
2016-05-25 21:10:07 -0300

    Pass twitter key and secret as config options
    - remove dotenv

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require('coffee-script/register');
-require('dotenv').load({silent: true});
 
 var pipeline = require('./lib/pipeline');
+var config   = require('./lib/config');
 
-module.exports = {
-  search       : pipeline.search,
-  stream       : {},
-  DEFAULT_QUERY: process.env.DEFAULT_QUERY
+exports.search = function(options, cb) {
+  set_config(options);
+  var query = (options.query === true) ? config.DEFAULT_QUERY : options.query;
+  pipeline.search.write(query);
+  cb(pipeline.search);
 };
+
+/**
+ * Set configuration options
+ */
+function set_config(options) {
+  if (options.key)    config.TWITTER_CONSUMER_KEY    = options.key;
+  if (options.secret) config.TWITTER_CONSUMER_SECRET = options.secret;
+}
