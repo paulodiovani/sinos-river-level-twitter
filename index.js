78ef7e2fa76f86959ea34f130dca03515bae7c6b
Paulo Diovani <paulo@diovani.com>
2016-05-25 23:40:22 -0300

    Drop pipelines (not needed)

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('coffee-script/register');
 
-var pipeline = require('./lib/pipeline');
-var config   = require('./lib/config');
+var config = require('./lib/config');
+var tweets = require('./lib/tweets');
+var parser = require('./lib/parser');
 
 exports.search = function(options, cb) {
   config.set(options);
   var query = (options.query === true) ? config.DEFAULT_QUERY : options.query;
-  pipeline.search.write(query);
-  cb(pipeline.search);
+  tweets.search.write(query);
+  cb(tweets.search.pipe(parser));
 };
